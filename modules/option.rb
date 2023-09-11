eee8ff2f630a0ffc80386e64f75346a2899d77b7
Anita Sharma <sharma.anita00001@gmail.com>
2023-09-11 20:40:59 +0545

    Module: List of methods

---
+++ modules/option.rb
@@ -0,0 +1,16 @@
+module Options
+  def select_option(user_choice)
+    {
+      '1' => :list_all_books,
+      '2' => :list_all_music_albums,
+      '3' => :list_all_games,
+      '4' => :list_all_genres,
+      '5' => :list_all_labels,
+      '6' => :list_all_authors,
+      '7' => :add_book,
+      '8' => :add_music_album,
+      '9' => :add_game,
+      '10' => :exit
+    }
+  end
+end
