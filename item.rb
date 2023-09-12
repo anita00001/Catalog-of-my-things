1531f60b65711bf0e7f5c1ccd982abb2d3e36c14
mosidrum <mosiokanga@gmail.com>
2023-09-12 03:54:46 +0100

    modify item class

--- item.rb
+++ item.rb
@@ -1,24 +1,17 @@
 class Item
-  attr_accessor :id, :genre, :author, :source, :label, :publish_date
-  attr_reader :archive
+  attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
 
-  def initialize(params)
-    @id = params[:id]
-    @genre = params[:genre]
-    @author = params[:author]
-    @source = params[:source]
-    @label = params[:label]
-    @publish_date = params[:publish_date]
-    @archive = params.fetch(:archive, false)
+  def initialize(id, publish_date)
+    @id = id
+    @publish_date = publish_date
+    @archived = false
   end
 
   def can_be_archived?
-    (Date.today - @publish_date).to_i > 3652
+    (Time.now.year - @publish_date.year) > 10
   end
 
   def move_to_archive
-    return unless can_be_archived?
-
-    @archived = true
+    @archived = can_be_archived?
   end
 end
