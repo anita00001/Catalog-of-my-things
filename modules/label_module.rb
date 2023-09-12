854f56fb165a919be2d2c232bf57ad98ae911b91
Anita Sharma <sharma.anita00001@gmail.com>
2023-09-12 12:02:18 +0545

    Methods to add book, add label, list books, and list labels

---
+++ modules/label_module.rb
@@ -0,0 +1,24 @@
+require_relative '../classes/label'
+
+module LabelModule
+  def create_label(item = nil)
+    puts 'Title:'
+    title = gets.chomp
+    puts 'Color:'
+    color = gets.chomp
+
+    if item.nil?
+      @labels << Label.new(title, color)
+    else
+      new_label = Label.new(title, color)
+      new_label.add_item(item)
+      @labels << new_label
+    end
+  end
+
+  def list_labels
+    @labels.each_with_index do |label, index|
+      puts "#{index + 1} #{label}"
+    end
+  end
+end
