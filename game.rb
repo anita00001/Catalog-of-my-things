1509638451f5c5bba08874976134bac554417325
mosidrum <mosiokanga@gmail.com>
2023-09-12 04:36:22 +0100

    Add preserve data method

--- game.rb
+++ game.rb
@@ -10,4 +10,32 @@ class Game < Item
   def can_be_archived?
     super && (Time.now - @last_played_at) > (365 * 2 * 24 * 60 * 60) # 2 years in seconds
   end
+
+  def self.load_from_json(file_path)
+    JSON.parse(File.read(file_path)).map do |game_data|
+      Game.new(
+        game_data['id'],
+        Date.parse(game_data['publish_date']),
+        game_data['multiplayer'],
+        Time.parse(game_data['last_played_at'])
+      )
+    end
+  end
+
+  def save_to_json(file_path)
+    game_data = {
+      'id' => @id,
+      'publish_date' => @publish_date.to_s,
+      'multiplayer' => @multiplayer,
+      'last_played_at' => @last_played_at.to_s
+    }
+
+    File.open(file_path, 'a') do |file|
+      file.puts(game_data.to_json)
+    end
+  end
+
+  def save
+    SaveDataToFile.save_to_json('games.json', [self])
+  end
 end
