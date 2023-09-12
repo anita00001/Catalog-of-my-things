309be90040a32286ed33545ad831b2a5bb07e8a4
mosidrum <mosiokanga@gmail.com>
2023-09-12 04:40:14 +0100

    Add preserve data method

--- music_album.rb
+++ music_album.rb
@@ -11,4 +11,20 @@ class MusicAlbum < Item
   def can_be_archived?
     super && @on_spotify
   end
+
+  def save_to_json(file_path)
+    music_album_data = {
+      'id' => @id,
+      'publish_date' => @publish_date.to_s,
+      'on_spotify' => @on_spotify
+    }
+
+    File.open(file_path, 'a') do |file|
+      file.puts(music_album_data.to_json)
+    end
+  end
+
+  def save
+    SaveDataToFile.save_to_json('music_albums.json', [self])
+  end
 end
