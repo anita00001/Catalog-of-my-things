bcc70b9db277ff38fae6a84c74ab4ac976d97f8c
Anita Sharma <sharma.anita00001@gmail.com>
2023-09-15 11:39:41 +0545

    Final Touch Up

--- classes/game.rb
+++ classes/game.rb
@@ -4,7 +4,7 @@ require 'json'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(_publish_date, multiplayer, last_played_at)
+  def initialize(publish_date, multiplayer, last_played_at)
     super(id)
     @id = id.nil? ? Random.rand(100..10_000) : id
     @publish_date = publish_date
@@ -17,13 +17,14 @@ class Game < Item
   end
 
   def to_s
-    "ID: #{@id}, Multiplayer: #{@multiplayer}, Last played date: #{@last_played_at}"
+    "ID: #{@id}, Published date: #{@publish_date}, Multiplayer: #{@multiplayer}, Last played date: #{@last_played_at}"
   end
 
   def to_json(*args)
     {
       JSON.create_id => self.class.name,
       'id' => @id,
+      'publish_date' => @publish_date,
       'multiplayer' => @multiplayer,
       'last_played_at' => @last_played_at
     }.to_json(*args)
